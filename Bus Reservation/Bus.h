e77c8b6a08072b489157328c25d8dad0c7e237cc
FlamingHub <53872662+FlamingHub@users.noreply.github.com>
2019-09-08 15:10:04 +0530

    Create Bus.h

---
+++ Bus Reservation/Bus.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+class Bus
+{
+	std::string driverName;
+	std::string destination;
+	std::string from;
+	std::string timeDepart;
+	std::string timeArrive;
+   std::string 
+public:
+	Bus(std::string drivername, std::string des = "", std::string from = "")
+		: driverName(drivername), destination(des), from(from) {}
+
+	void setTimeDepart(int hours, int minutes);
+
+	void setTimeArrive(int hours, int minutes);
+
+	void printDriverInfo();
+
+	// using 2d array for available seat
+	void printAvailableSeat();
+};
