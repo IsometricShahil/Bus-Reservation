dd6868106bf9b34fa101037deaa41171efa1a129
FlamingHub <53872662+FlamingHub@users.noreply.github.com>
2019-09-11 07:50:34 +0530

    seats array and formatted source

--- Bus Reservation/Bus.h
+++ Bus Reservation/Bus.h
@@ -8,17 +8,20 @@ class Bus
 	std::string from;
 	std::string timeDepart;
 	std::string timeArrive;
-   std::string seats[40]; //(5sq * 2 row) = 10sq*4seats = 40 seats
+	std::string seats[40];		// (5sq * 2 row) = 10sq*4seats = 40 seats
 
-public:
-	Bus(std::string drivername, std::string des = "", std::string from = "")
-		: driverName(drivername), destination(des), from(from) {}
+  public:
+  Bus(std::string drivername, std::string des = "", std::string from = ""):driverName(drivername), destination(des),
+		from(from)
+	{
+	}
 
-	void setTimeDepart(string hours, string minutes);
+	void setTimeDepart(std::string hours, std::string minutes);
 
-	void setTimeArrive(string hours, string minutes);
+	void setTimeArrive(std::string hours, std::string minutes);
 
 	void printBusInfo();
 
 	void printAvailableSeat();
 };
+
