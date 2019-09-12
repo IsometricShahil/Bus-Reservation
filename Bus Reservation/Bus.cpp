74fc4d900a056dbfd7af7d839544690d82fb4e41
FlamingHub <53872662+FlamingHub@users.noreply.github.com>
2019-09-12 20:54:14 +0530

    Update Bus.cpp

--- Bus Reservation/Bus.cpp
+++ Bus Reservation/Bus.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 
-Bus(std::string drivername, std::string des = "", std::string from = ""):driverName(drivername), destination(des),
+Bus::Bus(std::string drivername, std::string des, std::string from):driverName(drivername), destination(des),
 from(from)
 {
 	for (int i = 0; i < 40; i++)
@@ -11,7 +11,7 @@ from(from)
 	}
 }
 
-void Bus::setTimeDepart(string hours = "00", string minutes = "00")
+void Bus::setTimeDepart(std::string hours, std::string minutes)
 {
 	if (hours.length() == 1)
 	{
@@ -24,7 +24,7 @@ void Bus::setTimeDepart(string hours = "00", string minutes = "00")
 	this->timeDepart = hours + ":" + minutes;
 }
 
-void Bus::setTimeArrive(string hours = "00", string minutes = "00")
+void Bus::setTimeArrive(std::string hours, std::string minutes)
 {
 	if (hours.length() == 1)
 	{
@@ -46,7 +46,7 @@ void Bus::printBusInfo()
 	std::cout << "Departure\t: " << this->timeDepart << "\n";
 }
 
-void printAvaliableSeats()
+void Bus::printAvaliableSeats()
 {
 	bool nextLine = false;
 
@@ -54,17 +54,29 @@ void printAvaliableSeats()
 	{
 		std::cout << (i + 1) << ". " << seats[i];
 
-		if (i % 2 == 0 && nextLine)
+		if ((i + 1) % 2 == 0 && nextLine)
 		{
 			std::cout << std::endl;
+			nextLine = false;
 			continue;
 		}
-		if (i % 2 == 0 && !nextLine)
+		if ((i + 1) % 2 == 0 && !nextLine)
 		{
 			std::cout << "\t\t";
 			nextLine = true;
 			continue;
 		}
 
-		std::cout << "\t";
+		std::cout << "  ";
 	}
+}
+
+ void Bus::seatReserve(int sNum, std::string name)
+{
+	seats[sNum - 1] = name;
+}
+
+void Bus::cancelReserve(int sNum)
+{
+	seats[sNum - 1] = "Empty!";
+}
