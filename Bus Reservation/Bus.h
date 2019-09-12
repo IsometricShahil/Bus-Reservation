ebf9464c6df25d52cc18ee54f3d327e7df5869e0
FlamingHub <53872662+FlamingHub@users.noreply.github.com>
2019-09-12 20:53:44 +0530

    Update Bus.h
    Seats

--- Bus Reservation/Bus.h
+++ Bus Reservation/Bus.h
@@ -11,17 +11,19 @@ class Bus
 	std::string seats[40];		// (5sq * 2 row) = 10sq*4seats = 40 seats
 
   public:
-  Bus(std::string drivername, std::string des = "", std::string from = ""):driverName(drivername), destination(des),
-		from(from)
-	{
-	}
 
-	void setTimeDepart(std::string hours, std::string minutes);
+	Bus(std::string drivername, std::string des = "", std::string from = "");
 
-	void setTimeArrive(std::string hours, std::string minutes);
+	void setTimeArrive(std::string hours = "00", std::string minutes = "00" );
+
+	void setTimeDepart(std::string hours = "00", std::string minutes = "00");
 
 	void printBusInfo();
 
-	void printAvailableSeat();
+	void printAvaliableSeats();
+	
+	void seatReserve(int sNum , std::string name);
+	
+	void cancelReserve(int sNum);
+	
 };
-
